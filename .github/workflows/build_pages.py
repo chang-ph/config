d08efc60831ddd4cab9eeea4001bcc686b119b83
PanHongChang <phchang@qq.com>
2025-12-25 14:22:10 +0800

    update

---
+++ .github/workflows/build_pages.py
@@ -0,0 +1,23 @@
+import json
+import os
+from pathlib import Path
+
+import yaml
+
+BUILD_OUTPUT_FOLDER = os.environ["BUILD_OUTPUT_FOLDER"]
+project_root = Path(__file__).parent.parent.parent
+
+
+def main():
+    for file in Path(project_root).glob("config-*.yaml"):
+        if not file.is_file():
+            continue
+        config = yaml.safe_load(file.read_text())
+        json_str = json.dumps(config)
+        (Path(BUILD_OUTPUT_FOLDER) / file.with_suffix(".json").name).write_text(
+            json_str
+        )
+
+
+if __name__ == "__main__":
+    main()
